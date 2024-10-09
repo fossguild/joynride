739a24e5fa4ad4af8c6e1bc674f60582d7d351f5
Arthur Vergaças <arthurvdm@gmail.com>
2024-10-08 21:23:53 -0300

    feat: intial gin and docker setup

---
+++ serv/backend/main.go
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2024 The JoynRide Authors and CCOS-USP <https://ccos.icmc.usp.br>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package main
+
+import (
+	"github.com/fossguild/joynride/tree/dev/serv/api"
+)
+
+func main() {
+    api.RunApi()
+}
