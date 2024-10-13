0847c2b998203bc079504267d4edb0d6a64914a4
Arthur Vergaças <arthurvdm@gmail.com>
2024-10-12 22:52:37 -0300

    feat: use migrations to change the database

---
+++ serv/db/migrations/20241012181937_create_database_initial_structure.down.sql
@@ -0,0 +1,17 @@
+-- SPDX-FileCopyrightText: 2024 CCOS-USP <https://ccos.icmc.usp.br
+--
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+DROP TABLE place;
+
+DROP TABLE user_ride_rating;
+
+DROP TABLE user_ride;
+
+DROP TABLE ride;
+
+DROP TABLE user_vehicle;
+
+DROP TABLE vehicle;
+
+DROP TABLE "user";
