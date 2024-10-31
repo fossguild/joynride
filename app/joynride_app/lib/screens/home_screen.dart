2c51c7b142af30250316cd84e38130b864ca0986
Lucas Stapf <lucas.c.stapf@gmail.com>
2024-10-31 08:36:59 -0300

    fix: resolve background issue

--- app/joynride_app/lib/screens/home_screen.dart
+++ app/joynride_app/lib/screens/home_screen.dart
@@ -14,57 +14,59 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-    return Scaffold(
-      extendBody: true,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image:
-                AssetImage('assets/images/backgrounds/background_1_blue.png'),
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/images/backgrounds/background_1_blue.png'),
         ),
-        child: [const SearchScreen(), for(var i = 0; i < 4; i++) Placeholder()][_currentPageIndex],
       ),
-      bottomNavigationBar: Container(
-        clipBehavior: Clip.antiAlias,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(24),
-            topRight: Radius.circular(24),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: [
+          const SearchScreen(),
+          for (var i = 0; i < 4; i++) const Placeholder()
+        ][_currentPageIndex],
+        bottomNavigationBar: Container(
+          clipBehavior: Clip.antiAlias,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24),
+              topRight: Radius.circular(24),
+            ),
           ),
-        ),
-        child: NavigationBar(
-          indicatorColor: colorScheme.tertiary,
-          backgroundColor: Colors.white,
-          onDestinationSelected: (index) {
-            setState(() {
-              _currentPageIndex = index;
-            });
-          },
-          selectedIndex: _currentPageIndex,
-          destinations: const <Widget>[
-            NavigationDestination(
-              icon: Icon(Icons.place),
-              label: 'Procurar',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.commute),
-              label: 'Oferecer',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.local_taxi),
-              label: 'Caronas',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.inbox),
-              label: 'Mensagens',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.account_circle),
-              label: 'Perfil',
-            ),
-          ],
+          child: NavigationBar(
+            indicatorColor: colorScheme.tertiary,
+            backgroundColor: Colors.white,
+            onDestinationSelected: (index) {
+              setState(() {
+                _currentPageIndex = index;
+              });
+            },
+            selectedIndex: _currentPageIndex,
+            destinations: const <Widget>[
+              NavigationDestination(
+                icon: Icon(Icons.place),
+                label: 'Procurar',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.commute),
+                label: 'Oferecer',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.local_taxi),
+                label: 'Caronas',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.inbox),
+                label: 'Mensagens',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.account_circle),
+                label: 'Perfil',
+              ),
+            ],
+          ),
         ),
       ),
     );
