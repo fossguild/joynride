ce3767c9136f63eeb69373a797f5743e018e0286
Lucas Stapf <lucas.c.stapf@gmail.com>
2024-11-17 14:18:51 -0300

    ref: use text constants in OfferScreen and SearchScreen

---
+++ app/joynride_app/lib/utils/strings/offer_strings.dart
@@ -0,0 +1,6 @@
+abstract class OfferStrings {
+  static const String offerTitle = 'Ofereça sua carona com segurança';
+  static const String seatQuantity = 'Quantidade de Vagas';
+  static const String quickResevation = 'Reserva rápida';
+  static const String offerButton = 'Oferecer';
+}
---
+++ app/joynride_app/lib/utils/strings/search_strings.dart
@@ -0,0 +1,4 @@
+abstract class SearchStrings {
+  static const String searchTitle = 'Busque sua carona com segurança';
+  static const String searchButton = 'Procurar';
+}
