39cc6fed645e0d8af4ea47f150e5f60331bc172d
Lucas Stapf <lucas.c.stapf@gmail.com>
2024-11-20 12:02:31 -0300

    code: add configuration to enable widget testing

--- app/joynride_app/lib/main.dart
+++ app/joynride_app/lib/main.dart
@@ -8,6 +8,47 @@ void main() {
   runApp(const MyApp());
 }
 
+class WidgetTest extends StatelessWidget {
+  const WidgetTest(this.widget, {super.key});
+
+  final Widget widget;
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme.copyWith(
+          primary: primaryBlue,
+          secondary: secondaryBlue,
+          tertiary: yellow,
+          onPrimary: Colors.white,
+          onSecondary: Colors.white,
+          onTertiary: Colors.white,
+        );
+
+    final textTheme = Theme.of(context).textTheme.apply(
+          fontFamily: GoogleFonts.roboto().fontFamily,
+          displayColor: grayBlue,
+          bodyColor: grayBlue,
+        );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        // margin: const EdgeInsets.all(8),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: widget,
+        ),
+      ),
+      theme: ThemeData.from(
+        colorScheme: colorScheme,
+        textTheme: textTheme,
+        useMaterial3: true,
+      ),
+    );
+  }
+}
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
